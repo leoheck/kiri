046e33bae9baeb22323b404ddd56792579b9926a
Leandro Heck <leoheck@gmail.com>
2021-11-18 22:41:02 -0300

    Update

--- install_kiri.sh
+++ install_kiri.sh
@@ -8,7 +8,7 @@ rm -rf $HOME/kiri
 
 if which git &> /dev/null; then
 
-	git clone https://github.com/leoheck/kiri.git $HOME/kriri
+	git clone https://github.com/leoheck/kiri.git $HOME/kiri
 
 else
 
