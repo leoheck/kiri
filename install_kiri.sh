ddaf24463981548f26c3c477d30e3125ccecc643
Leandro Heck <leoheck@gmail.com>
2021-11-18 22:40:20 -0300

    Update

--- install_kiri.sh
+++ install_kiri.sh
@@ -33,7 +33,7 @@ esac
 
 mkdir -p ${KICAD_PLUGINS_PATH}
 rm -rf ${KICAD_PLUGINS_PATH}/kiri
-cp -r kicad_plugin ${KICAD_PLUGINS_PATH}/kiri
+cp -r $HOME/kiri/kicad_plugin ${KICAD_PLUGINS_PATH}/kiri
 
 echo
 echo "Add the follwing lines to your ~/.bashrc or ~/.zshrc"
