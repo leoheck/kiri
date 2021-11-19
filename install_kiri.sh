3193c69850d04c43b8d0af0b70e22951a9b5714d
Leandro Heck <leoheck@gmail.com>
2021-11-18 22:26:34 -0300

    Update install script

--- install_kiri.sh
+++ install_kiri.sh
@@ -3,6 +3,15 @@
 # Install Kicad Plugin
 # To work, kiri must be in PATH
 
+# Donwload repo zip
+curl https://github.com/leoheck/kiri/archive/refs/heads/main.zip -O /tmp/kiri.zip
+
+# Remove old if any
+rm -rf $HOME/kiri
+
+unzip /tmp/kiri.zip -d $HOME/
+rm -rf kiri.zip
+
 case $OSTYPE in
 	darwin*)
 		KICAD_PLUGINS_PATH="$HOME/Library/Preferences/kicad/scripting/plugins"
@@ -15,3 +24,9 @@ esac
 mkdir -p ${KICAD_PLUGINS_PATH}
 rm -rf ${KICAD_PLUGINS_PATH}/kiri
 cp -r kicad_plugin ${KICAD_PLUGINS_PATH}/kiri
+
+echo
+echo "Add the follwing lines to your ~/.bashrc or ~/.zshrc"
+echo "source \$HOME/kiri/env.sh"
+echo "source \$HOME/kiri/submodules/KiCad-Diff/env.sh"
+echo
