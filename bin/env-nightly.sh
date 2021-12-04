970813dee54768a0ce9597c6e5006011729f1f76
Leandro Heck <leoheck@gmail.com>
2021-12-04 11:27:02 -0300

    Detect script path

--- bin/env-nightly.sh
+++ bin/env-nightly.sh
@@ -2,6 +2,46 @@
 
 # Setup for Kicad-Nigtly (>= 5.99) python modules
 
+readlink_osx()
+{
+	target_file="$1"
+	dir_name=$(dirname "$target_file")
+
+	cd "${dir_name}" || exit
+	target_file=$(basename "$target_file")
+
+	# Iterate down a (possible) chain of symlinks
+	while [ -L "$target_file" ]
+	do
+		target_file=$(readlink "$target_file")
+		cd "$(dirname "$target_file")" || exit
+		target_file=$(basename "$target_file")
+	done
+
+	# Compute the canonicalized name by finding the physical path
+	# for the directory we're in and appending the target file.
+	phys_dir=$(pwd -P)
+
+	result="$phys_dir/$target_file"
+
+	echo "$result"
+}
+
+readlink_()
+{
+	case $OSTYPE in
+		darwin*)
+			readlink_osx "$1"
+			;;
+		*)
+			readlink -f "$1"
+			;;
+	esac
+}
+
+script=$(readlink_ "$0")
+script_path=$(dirname "$script")
+
 case ${OSTYPE} in
 
 	# MaxOS (update to your Kicad nightly path)
@@ -18,4 +58,4 @@ case ${OSTYPE} in
 
 esac
 
-source ./env.sh
+source ${script_path}/env.sh
