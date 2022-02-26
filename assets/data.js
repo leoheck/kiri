39adcde0428be71b7d6e470af8a03964fe74bd5a
Leandro Heck <leoheck@gmail.com>
2022-02-26 01:08:57 -0300

    Add new data.js file

---
+++ assets/data.js
@@ -0,0 +1,64 @@
+
+// This file should be recreated with the kiri script
+// This is just a template
+
+const data_json = '{ \
+	"local": \
+	{ \
+		"description": "This commit does this.", \
+		"pages": \
+		{ \
+			"board": "board.sch" \
+		}, \
+		"page_imgs": \
+		{ \
+			"board": "../local/board-sch.svg" \
+		}, \
+		"layers": \
+		{ \
+			 "0": "F.Cu", \
+			"31": "B.Cu", \
+			"32": "B.Adhes", \
+			"33": "F.Adhes", \
+			"34": "B.Paste", \
+			"35": "F.Paste", \
+			"36": "B.SilkS", \
+			"37": "F.SilkS", \
+			"38": "B.Mask", \
+			"39": "F.Mask", \
+			"40": "Dwgs.User", \
+			"41": "Cmts.User", \
+			"42": "Eco1.User", \
+			"43": "Eco2.User", \
+			"44": "Edge.Cuts", \
+			"45": "Margin", \
+			"46": "B.Courtyard", \
+			"47": "F.Courtyard", \
+			"48": "B.Fab", \
+			"49": "F.Fab" \
+		}, \
+		"layer_imgs": \
+		{ \
+			 "0": "../local/board-F_Cu.svg", \
+			"31": "../local/board-B_Cu.svg", \
+			"32": "../local/board-B_Adhes.svg", \
+			"33": "../local/board-F_Adhes.svg", \
+			"34": "../local/board-B_Paste.svg", \
+			"35": "../local/board-F_Paste.svg", \
+			"36": "../local/board-B_SilkS.svg", \
+			"37": "../local/board-F_SilkS.svg", \
+			"38": "../local/board-B_Mask.svg", \
+			"39": "../local/board-F_Mask.svg", \
+			"40": "../local/board-Dwgs_User.svg", \
+			"41": "../local/board-Cmts_User.svg", \
+			"42": "../local/board-Eco1_User.svg", \
+			"43": "../local/board-Eco2_User.svg", \
+			"44": "../local/board-Edge_Cuts.svg", \
+			"45": "../local/board-Margin.svg", \
+			"46": "../local/board-B_Courtyard.svg", \
+			"47": "../local/board-F_Courtyard.svg", \
+			"48": "../local/board-B_Fab.svg", \
+			"49": "../local/board-F_Fab.svg" \
+		} \
+	} \
+}';
