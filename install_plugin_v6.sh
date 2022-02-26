c7408cb121e3b7338a77dae3e41ad43a9e498283
Damien LaRocque <phicoltan@gmail.com>
2022-02-26 18:20:23 -0500

    Correct paths and variable names in install script v6

--- install_plugin_v6.sh
+++ install_plugin_v6.sh
@@ -6,9 +6,9 @@
 KIRI_REPO_PATH=$1
 
 if [ -z "${KIRI_REPO_PATH}" ]; then
-	REPO_PATH=.
+	KIRI_REPO_PATH=.
 else
-	REPO_PATH="${KIRI_REPO_PATH}"
+	KIRI_REPO_PATH="${KIRI_REPO_PATH}"
 fi
 
 case $OSTYPE in
@@ -20,7 +20,7 @@ case $OSTYPE in
 		;;
 esac
 
-if [[ -d ${HOME}/$HOME/.local/share/kicad/6.0/ ]]; then
+if [[ -d "$HOME/.local/share/kicad/6.0/" ]]; then
 	mkdir -p "${KICAD_PLUGINS_PATH}"
 	rm -rf "${KICAD_PLUGINS_PATH}/kiri"
 	cp -r "${KIRI_REPO_PATH}/kicad/plugin/kiri_v6" "${KICAD_PLUGINS_PATH}/kiri"
